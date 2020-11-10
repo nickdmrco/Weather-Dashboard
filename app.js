0cd86e54f5d44e04856d28c5d3601a24fe236611
Nick De Marco <nickdmrco@gmail.com>
2020-11-10 13:04:04 -0800

    commit

--- app.js
+++ app.js
@@ -45,3 +45,31 @@ $("#searchBtn").on("click", function () {
 
   })
 });
+
+function makeList() {
+ let listItem = $("<li>").addClass("list-group-item").text(city);
+ $(".list").append(listItem);
+}
+
+function getCurrentConditions(response) {
+
+ let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+ tempF = Math.floor(tempF);
+
+ $('#currentCity').empty();
+
+ const card = $("<div>").addClass("card");
+ const cardBody = $("<div>").addClass("card-body");
+ const city = $("<h4>").addClass("card-title").text(response.name);
+ const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+ const temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+ const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+ const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+ const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+
+ city.append(cityDate, image)
+ cardBody.append(city, temperature, humidity, wind);
+ card.append(cardBody);
+ $("#currentCity").append(card)
+
+}
