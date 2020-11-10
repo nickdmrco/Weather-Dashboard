67fa6e2f9bf1fcb9e5dc692c3819f086b274191f
Nick De Marco <nickdmrco@gmail.com>
2020-11-10 13:04:28 -0800

    Final js

--- app.js
+++ app.js
@@ -73,3 +73,48 @@ function getCurrentConditions(response) {
  $("#currentCity").append(card)
 
 }
+
+function getCurrentForecast() {
+
+ $.ajax({
+  url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
+  method: "GET"
+ }).then(function (response) {
+
+  console.log(response)
+  console.log(response.dt)
+  $('#forecast').empty();
+
+  let results = response.list;
+  console.log(results)
+
+
+  for (let i = 0; i < results.length; i++) {
+
+   let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+   let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
+   console.log(day);
+   console.log(hour);
+
+   if (results[i].dt_txt.indexOf("12:00:00") !== -1) {
+
+    let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+    let tempF = Math.floor(temp);
+
+    const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+    const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+    const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+    const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+    const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+
+    const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+
+    cardBody.append(cityDate, image, temperature, humidity);
+    card.append(cardBody);
+    $("#forecast").append(card);
+
+   }
+  }
+ });
+
+}
